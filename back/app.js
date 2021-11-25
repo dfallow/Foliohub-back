1399ab2b39f2f89c63d1b28ab26d0101f98643f2
root <root@davidfa-003.metropolia.fi>
2021-11-25 23:03:27 +0200

    post project and delete work

--- back/app.js
+++ back/app.js
@@ -1,15 +1,18 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const projectRoute = require('./routes/projectRoute');
 const app = express();
 const port = 3001;
 
 app.use(cors());
-app.use('/project', projectRoute);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use('/project', projectRoute);
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}! http://localhost:${port}`));
