ef706c7d0a104d7bfb69e4d88bfcb6431da41846
samhamal <samhamal@metropolia.fi>
2021-11-29 15:33:51 +0200

    project getting by role

--- back/controllers/authController.js
+++ back/controllers/authController.js
@@ -16,6 +16,7 @@ const login = (req, res, next) => {
                 return;
             }
             const token = jwt.sign(user, 'asdhjfkljeklwnflhldls');
+            req.user = user;
             return res.json({ user, token });
         });
     })(req, res, next);
