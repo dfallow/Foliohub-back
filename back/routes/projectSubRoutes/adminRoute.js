8cc764bfbc32bd79d3edf54d42da8c15b6c559ab
samhamal <samhamal@metropolia.fi>
2021-11-29 23:47:18 +0200

    database path fix

--- back/routes/projectSubRoutes/adminRoute.js
+++ back/routes/projectSubRoutes/adminRoute.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
+const {project_list_get_admin, project_delete_admin, project_get_admin, project_post_admin, project_update_admin} = require('../../controllers/projectSubControllers/adminController')
 
 router.route('/')
     .get(project_list_get_admin)
