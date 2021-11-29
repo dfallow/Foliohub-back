efc8a97649b40093e0a1fab295f8172ec88c6ae5
David Fallow <davidfa@metropolia.fi>
2021-11-29 17:46:17 +0200

    home page fetches and displays list of projects with no styling

--- front/js/home.js
+++ front/js/home.js
@@ -1,5 +1,5 @@
 'use strict';
-const url = 'http://10.114.32.29'; //should be server address
+const url = 'http://10.114.32.29/foliohub'; //should be server address
 
 //selecting html element
 const ul = document.querySelector('#projectList');
@@ -11,11 +11,11 @@ const createProjectList = (projects) => {
 
     projects.forEach((project) => {
         //creating li element with DOM methods
-        const img = document.createElement('img');
-        img.src = url + '/' + project.filename;
-        img.alt = project.name;
+        //const img = document.createElement('img');
+        //img.src = url + '/' + project.filename;
+        //img.alt = project.name;
 
-        const figure = document.createElement('figure').appendChild(img);
+        //const figure = document.createElement('figure').appendChild(img);
 
         const h3 = document.createElement('h3');
         h3.innerHTML = project.name;
@@ -28,9 +28,10 @@ const createProjectList = (projects) => {
 
         const li = document.createElement('li');
 
-        li.appendChild(figure);
+        //li.appendChild(figure);
         li.appendChild(h3);
         li.appendChild(viewBtn);
+        ul.appendChild(li);
     });
 };
 
@@ -39,6 +40,7 @@ const getProject = async () => {
     try {
         const response = await fetch(url + '/project');
         const projects = await response.json();
+        console.log(projects)
         createProjectList(projects);
     } catch (e) {
         console.log(e.message);
