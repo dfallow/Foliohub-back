81d50d6c8736fe49dd10f3453e8b1aecbacc8879
soulyvap <soulyvap@metropolia.fi>
2021-12-09 18:23:49 +0200

    user create validation

--- back/routes/userRoute.js
+++ back/routes/userRoute.js
@@ -3,18 +3,43 @@ const express = require('express');
 const passport = require('../utils/pass');
 const {user_list_get, user_post, user_get, user_delete, user_update, checkToken} = require("../controllers/userController");
 const router = express.Router();
+const {body} = require('express-validator');
 const multer = require("multer");
-const upload = multer({dest: './uploads/user'});
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype.includes('image')) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+        return cb(new Error('Only images allowed'))
+    }
+}
+const upload = multer({dest: './uploads/user', fileFilter});
 
 router.get('/token', checkToken);
 router.route('/')
     .get(user_list_get)
     .post(
         upload.single('profilePic'),
+        body('email').notEmpty().isEmail(),
+        body('password').notEmpty().isLength({min: 6}),
+        body('username').notEmpty().isLength({max: 30}),
+        body('creationDate').notEmpty(),
+        body('github').isURL(),
+        body('description').isLength({max: 60}),
+        body('tags').isLength({max: 35}),
+        body('title').isLength({max: 15}),
         user_post)
     .put(
         passport.authenticate('jwt', {session: false}),
         upload.single('profilePic'),
+        body('email').isEmail(),
+        body('password').notEmpty().isLength({min:6}),
+        body('username').notEmpty().isLength({max: 30}),
+        body('creationDate').notEmpty(),
+        body('github').isURL(),
+        body('description').isLength({max: 60}),
+        body('tags').isLength({max: 35}),
+        body('title').isLength({max: 15}),
         user_update)
     .delete(passport.authenticate('jwt', {session: false}), user_delete)
 
